583d7192fa83a27f56846d67a482e0d6637dd0f8
Paul K <spkim0921@gmail.com>
2025-03-02 11:44:35 -0800

    feat: building user chats router

--- routes/chats.ts
+++ routes/chats.ts
@@ -5,32 +5,60 @@ import { userChats as userChatsTable } from "../schemas/userchats";
 import { mightFail } from "might-fail";
 import { db } from "../db";
 import { HTTPException } from "hono/http-exception";
+import { eq } from "drizzle-orm";
+import { chats as chatsTable } from "../schemas/chats";
 
-export const userChatsRouter = new Hono().post(
-  "/",
-  zValidator(
-    "json",
-    createInsertSchema(userChatsTable).omit({
-      userChatId: true,
-      createdAt: true,
-    })
-  ),
-  async (c) => {
-    const insertValues = c.req.valid("json");
-    const { error: userChatInsertError, result: userChatInsertResult } =
+export const userChatsRouter = new Hono()
+  .post(
+    "/",
+    zValidator(
+      "json",
+      createInsertSchema(userChatsTable).omit({
+        userChatId: true,
+        createdAt: true,
+      })
+    ),
+    async (c) => {
+      const insertValues = c.req.valid("json");
+      const { error: userChatInsertError, result: userChatInsertResult } =
+        await mightFail(
+          db
+            .insert(userChatsTable)
+            .values({ ...insertValues })
+            .returning()
+        );
+      if (userChatInsertError) {
+        console.log("Error while creating chat");
+        throw new HTTPException(500, {
+          message: "Error while creating chat",
+          cause: userChatInsertResult,
+        });
+      }
+      return c.json({ user: userChatInsertResult[0] }, 200);
+    }
+  )
+  .get("/:userId", async (c) => {
+    const userIdString = c.req.param("userId");
+    if (!userIdString) {
+      return c.json({ error: "userId parameter is required." }, 400);
+    }
+    const { result: userChatsQueryResult, error: userChatsQueryError } =
       await mightFail(
         db
-          .insert(userChatsTable)
-          .values({ ...insertValues })
-          .returning()
+          .select({
+            chatId: chatsTable.chatId,
+            title: chatsTable.title,
+            createdAt: chatsTable.createdAt,
+          })
+          .from(userChatsTable)
+          .innerJoin(chatsTable, eq(userChatsTable.chatId, chatsTable.chatId))
+          .where(eq(userChatsTable.userId, userIdString))
       );
-    if (userChatInsertError) {
-      console.log("Error while creating chat");
+    if (userChatsQueryError) {
       throw new HTTPException(500, {
-        message: "Error while creating chat",
-        cause: userChatInsertResult,
+        message: "Error occurred when fetching user chats.",
+        cause: userChatsQueryError,
       });
     }
-    return c.json({ user: userChatInsertResult[0] }, 200);
-  }
-);
+    return c.json({ chats: userChatsQueryResult });
+  });
