c21b82149eb96a8ded273f3783799d37c820a9f0
Paul K <102385105+codingcapy@users.noreply.github.com>
2025-03-21 11:53:23 -0700

    feat: build frontend to edit profile pic

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,9 +1,22 @@
 import { CgProfile } from "react-icons/cg";
 import profilePic from "/capypaul01.jpg";
 import useAuthStore from "../store/AuthStore";
+import { useState } from "react";
 
 export default function Profile() {
   const { user } = useAuthStore();
+  const [preview, setPreview] = useState<string | null>(null);
+
+  async function handleImageUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
   return (
     <div className="md:w-[55%] md:border-r md:h-screen overflow-auto">
@@ -13,9 +26,20 @@ export default function Profile() {
       </div>
       <div className="p-5 pt-[70px]">
         <img
-          src={profilePic}
+          src={preview ? preview : profilePic}
           className="max-w-30 md:max-w-xs rounded-full mx-auto pb-2"
         />
+        <div className="mb-5">
+          <label className="border-2 border-cyan-600 text-cyan-600 font-bold px-5 py-2 my-5 w-[300px] mx-auto rounded hover:bg-cyan-600 hover:text-black ease-in-out duration-300">
+            Edit Profile Pic
+            <input
+              type="file"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageUpload}
+            />
+          </label>
+        </div>
         <p className="my-1 md:my-3">Username: {user && user.username}</p>
         <p className="my-1 md:my-3">
           Member Since: {user && user.createdAt.toString().slice(0, 10)}
